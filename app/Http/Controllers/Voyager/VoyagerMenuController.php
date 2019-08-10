91b66d2ef1705e92f55b9e2e9835f6c9267ccfa4
Hamza <hamzabitup@gmail.com>
2019-08-10 14:32:59 +0100

    changes

--- app/Http/Controllers/Voyager/VoyagerMenuController.php
+++ app/Http/Controllers/Voyager/VoyagerMenuController.php
@@ -7,4 +7,21 @@ use TCG\Voyager\Http\Controllers\VoyagerMenuController as BaseVoyagerMenuControl
 class VoyagerMenuController extends BaseVoyagerMenuController
 {
     //
+    public function __construct()
+    {
+
+        // for some reasn when we change the locale
+        // it reverts back to its previous state 
+        // se localisation controller and middleware
+        // I digured adding this bit of code solves the 
+        // problem, a bit dirty but it works
+        $this->middleware(function ($request, $next) {
+            // fetch session and use it in entire class with constructor
+            if (session()->has('locale')) {
+                app()->setLocale(session()->get('locale'));
+            }
+
+            return $next($request);
+        });
+    }
 }
