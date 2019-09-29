af5e0ee55748d31d8534f4e74c7daa3b362cd7e3
Hamza <hamzabitup@gmail.com>
2019-09-29 23:20:13 +0100

    fixing the issue

--- resources/views/layouts/pdf.blade.php
+++ resources/views/layouts/pdf.blade.php
@@ -1,15 +1,7 @@
 <!DOCTYPE html>
 <html lang="{{ config('app.locale') }}" dir="{{ __('voyager::generic.is_rtl') == 'true' ? 'rtl' : 'ltr' }}">
 <head>
-    <title>@yield('page_title', setting('admin.title') . " - " . setting('admin.description'))</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="assets-path" content="{{ route('voyager.voyager_assets') }}"/>
-
-    <!-- Google Fonts -->
-    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,500,700" rel="stylesheet">
-
-    <!-- App CSS -->
-    <link rel="stylesheet" href="{{ voyager_asset('css/app.css') }}">
 </head>
 
 <body class="voyager">
