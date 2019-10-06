4f0c3aac383d0c4033891c571eeb9beb85fc28f6
Hamza <hamzabitup@gmail.com>
2019-10-06 23:57:23 +0100

    working on reports controller

--- app/Http/Controllers/Reports/ReportsSalesOrdersController.php
+++ app/Http/Controllers/Reports/ReportsSalesOrdersController.php
@@ -4,19 +4,32 @@ namespace App\Http\Controllers\Reports;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\SalesOrder;
 
 class ReportsSalesOrdersController extends Controller
 {
     public function index(Request $request) {
         $timeframe = (string)$request->query('timeframe');
-
+        
         switch ($timeframe) {
             case 'month':
                 # code...
                 break;
-            
-            default:
+
+            case 'quarter':
+                # get start and end month of the quarter
+                break;
+
+            case 'month':
                 # code...
+                break;
+
+            case 'month':
+                # code...
+                break;
+
+            default:
+                # raise an exception or error
                 break;
         }
         return 'test';
